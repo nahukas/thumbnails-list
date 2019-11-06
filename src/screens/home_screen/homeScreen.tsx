9f580976a477ae9325512d895a049cbc75c83597
Nahuel Castro <nahukas@gmail.com>
2019-11-06 20:16:54 -0300

    #2 Finished feature. Fetch thumnails

--- src/screens/home_screen/homeScreen.tsx
+++ src/screens/home_screen/homeScreen.tsx
@@ -1,17 +1,70 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  ViewStyle,
+  Text,
+  View,
+  ActivityIndicator,
+} from 'react-native';
 
-class HomeScreen extends React.Component<{}> {
+export interface Ithumbnail {
+  albumId: string;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+interface Style {
+  container: ViewStyle;
+  title: ViewStyle;
+}
+
+interface HomeScreenState {
+  isLoading: boolean;
+  thumbnails: any[];
+}
+
+interface HomeScreenProps {}
+
+class HomeScreen extends React.Component<HomeScreenProps, HomeScreenState> {
+  constructor(props: HomeScreenProps) {
+    super(props);
+    this.state = {
+      thumbnails: [],
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchThumbnails();
+  }
+
+  private fetchThumbnails = async () => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(json => this.setState({thumbnails: json, isLoading: false}));
+  };
+
   render() {
+    const {isLoading, thumbnails} = this.state;
     return (
-      <View>
-        <Text>Home Screen</Text>
+      <View style={styles.container}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <View>
+            {thumbnails.map(thumbnail => (
+              <Text key={thumbnail.id}>{thumbnail.title}</Text>
+            ))}
+          </View>
+        )}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create<Style>({
   container: {
     flex: 1,
     justifyContent: 'center',
